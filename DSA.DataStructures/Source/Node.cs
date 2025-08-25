bd1cfcb1501b20776db939dbf0fff9333a1efab7
danial pezeshpoor <dpezeshpoor@yahoo.com>
2025-08-25 09:53:47 +0330

    new form of node is implemented to use in Binary Trees

--- DSA.DataStructures/Source/Node.cs
+++ DSA.DataStructures/Source/Node.cs
@@ -24,7 +24,9 @@ public class BidirectionalNode
     public BidirectionalNode? Next { get; internal set; }
     public int Data { get; internal set; }
 
-    public BidirectionalNode(int data, BidirectionalNode? previous, BidirectionalNode? next)
+    public BidirectionalNode(int data,
+                            BidirectionalNode? previous,
+                            BidirectionalNode? next)
     {
         Data = data;
         Previous = previous;
@@ -34,5 +36,19 @@ public class BidirectionalNode
     public override string ToString()
     {
         return $"{Data}";
+    }
+}
+
+public class TreeNode
+{
+    public int Data { get; internal set; }
+    public TreeNode? Left { get; internal set; }
+    public TreeNode? Right { get; internal set; }
+
+    public TreeNode(int value, TreeNode? left, TreeNode? right)
+    {
+        Data = value;
+        Left = left;
+        Right = right;
     }
 }
