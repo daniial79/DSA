a0f08e33927b36b0474af72fd20a77b3d44e6963
danial pezeshpoor <dpezeshpoor@yahoo.com>
2025-09-11 13:59:56 +0330

    display method for heap is implemented

--- DSA.Playground/Program.cs
+++ DSA.Playground/Program.cs
@@ -7,6 +7,27 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        Heap myHeap = new Heap(10);
+
+        myHeap.Display();
+
+        myHeap.Insert(20);
+        myHeap.Display();
+        
+        myHeap.Insert(14);
+        myHeap.Display();
+
+        myHeap.Insert(2);
+        myHeap.Display();
+
+        myHeap.Insert(15);
+        myHeap.Display();
+
+        myHeap.Insert(200);
+        myHeap.Display();
+        
+        
+        
 
     }
 }
